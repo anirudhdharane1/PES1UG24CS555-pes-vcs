05404037332b023f007bf182cdc6509242fc64cc
anirudhdharane1 <dharaneanirudh@gmail.com>
2026-04-17 21:06:18 +0530

    Have added the qsort and fopen functions

--- index.c
+++ index.c
@@ -180,11 +180,25 @@ int index_load(Index *index) {
 //
 // Returns 0 on success, -1 on error.
 int index_save(const Index *index) {
-    // TODO: Implement atomic index saving
-    // (See Lab Appendix for logical steps)
-    (void)index;
-    return -1;
-}
+    Index copy = *index;
+    qsort(copy.entries, copy.count, sizeof(IndexEntry), cmp_index);
+
+    FILE *f = fopen(INDEX_FILE ".tmp", "w");
+    if (!f) return -1;
+
+    for (int i = 0; i < copy.count; i++) {
+        char hex[HASH_HEX_SIZE + 1];
+        hash_to_hex(&copy.entries[i].hash, hex);
+
+        fprintf(f, "%o %s %llu %u %s\n",
+                copy.entries[i].mode,
+                hex,
+                copy.entries[i].mtime_sec,
+                copy.entries[i].size,
+                copy.entries[i].path);
+    }
+
+
 
 // Stage a file for the next commit.
 //
