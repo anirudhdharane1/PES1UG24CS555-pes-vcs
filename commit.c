38be2c7c1da4be45d68b93ba325b429442a76e7c
anirudhdharane1 <dharaneanirudh@gmail.com>
2026-04-19 18:44:57 +0530

    I have added the had_read function

--- commit.c
+++ commit.c
@@ -194,8 +194,19 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
-}
+    Commit c;
+
+    if (tree_from_index(&c.tree) != 0)
+        return -1;
+
+    if (head_read(&c.parent) == 0)
+        c.has_parent = 1;
+    else
+        c.has_parent = 0;
+
+    snprintf(c.author, sizeof(c.author), "%s", pes_author());
+    c.timestamp = (uint64_t)time(NULL);
+    snprintf(c.message, sizeof(c.message), "%s", message);
+
+    void *raw;
+    size_t raw_len;
