c160da1d8cbbebd1d02ef80ea9919a3df34a18f3
anirudhdharane1 <dharaneanirudh@gmail.com>
2026-04-19 18:47:38 +0530

    added the commit_serialize and object_write function

--- commit.c
+++ commit.c
@@ -210,3 +210,11 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
 
     void *raw;
     size_t raw_len;
+    
+    if (commit_serialize(&c, &raw, &raw_len) != 0)
+        return -1;
+
+    if (object_write(OBJ_COMMIT, raw, raw_len, commit_id_out) != 0) {
+        free(raw);
+        return -1
+    }
